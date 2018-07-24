5620c791c13e691486314be5ee2b727af0687901
vandeb69 <br.vandenberghe@gmail.com>
2018-07-24 15:16:29 +0200

    first commit

--- loss.py
+++ loss.py
@@ -27,4 +27,78 @@ class get_frame_loss():
             return loss
 
     def compute_loss(self, frame0, transformed_image):
-        pass
+        loss = tf.reduce_mean(tf.abs(tf.subtract(frame0, transformed_image)))
+        return loss
+
+    def _interpolate(self, image, x, y, output_size):
+        batch_size = tf.shape(image)[0]
+        height = 128
+        width = 384
+        num_channels = tf.shape(image)[3]
+
+        x = tf.cast(x, dtype='float32')
+        y = tf.cast(y, dtype='float32')
+
+        height_float = tf.cast(height, dtype='float32')
+        width_float = tf.cast(width, dtype='float32')
+
+        output_height = output_size[0]
+        output_width = output_size[1]
+
+        x = x*(width_float)
+        y = y*(height_float)
+
+        x0 = tf.cast(tf.floor(x), 'int32')
+        x1 = x0 + 1
+        y0 = tf.cast(tf.floor(y), 'int32')
+        y1 = y0 + 1
+
+        max_y = tf.cast(height - 1, dtype='int32')
+        max_x = tf.cast(width - 1, dtype='int32')
+
+        zero = tf.zeros([], dtype='int32')
+
+        x0 = tf.clip_by_value(x0, zero, max_x)
+        x1 = tf.clip_by_value(x1, zero, max_x)
+        y0 = tf.clip_by_value(y0, zero, max_y)
+        y1 = tf.clip_by_value(y1, zero, max_y)
+
+        flat_image_dimensions = width * height
+
+        pixels_batch = tf.range(batch_size) * flat_image_dimensions
+        flat_output_dimensions = output_height * output_width
+        base = self._repeat(pixels_batch, flat_output_dimensions)
+        base_y0 = base + y0 * width
+        base_y1 = base + y1 * width
+        indices_a = base_y0 + x0
+        indices_b = base_y1 + x0
+        indices_c = base_y0 + x1
+        indices_d = base_y1 + x1
+
+        flat_image = tf.reshape(image, shape=(-1, num_channels))
+        flat_image = tf.cast(flat_image, dtype='float32')
+        pixel_values_a = tf.gather(flat_image, indices_a)
+        pixel_values_b = tf.gather(flat_image, indices_b)
+        pixel_values_c = tf.gather(flat_image, indices_c)
+        pixel_values_d = tf.gather(flat_image, indices_d)
+
+        x0 = tf.cast(x0, 'float32')
+        x1 = tf.cast(x1, 'float32')
+        y0 = tf.cast(y0, 'float32')
+        y1 = tf.cast(y1, 'float32')
+
+        area_a = tf.expand_dims(((x1 - x) * (y1 - y)), 1)
+        area_b = tf.expand_dims(((x1 - x) * (y - y0)), 1)
+        area_c = tf.expand_dims(((x - x0) * (y1 - y)), 1)
+        area_d = tf.expand_dims(((x - x0) * (y - y0)), 1)
+        output = tf.add_n([area_a * pixel_values_a,
+                           area_b * pixel_values_b,
+                           area_c * pixel_values_c,
+                           area_d * pixel_values_d])
+        return output
+
+    def _repeat(self, x, num_repeats):
+        ones = tf.ones((1, num_repeats), dtype='int32')
+        x = tf.reshape(x, shape=(-1, 1))
+        x = tf.matmul(x, ones)
+        return tf.reshape(x, [-1])
