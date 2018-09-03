e595d771995fc220c4dab8ab55e5c86ef4fd5316
vandeb69 <br.vandenberghe@gmail.com>
2018-09-03 11:30:31 +0900

    working DeepTesla_SfmNetLoader()

--- models/sfm_net_model.py
+++ models/sfm_net_model.py
@@ -30,39 +30,40 @@ class SfmNetModel(BaseModel):
             # self.I_t0 = tf.placeholder(tf.float32, (None, img_h, img_w, img_c), name='frame_t0')
             # self.I_t1 = tf.placeholder(tf.float32, (None, img_h, img_w, img_c), name='frame_t1')
 
-        self.f_point_cloud_1, self.f_depth_output = structure_net(self.I_t0)
-        self.b_point_cloud_1, self.b_depth_output = structure_net(self.I_t1, reuse=tf.AUTO_REUSE)
+        self.f_point_cloud_1, self.f_depth_output = structure_net(self.I_t0, is_training=self.is_training)
+        self.b_point_cloud_1, self.b_depth_output = structure_net(self.I_t1, is_training=self.is_training, reuse=tf.AUTO_REUSE)
 
         self.f_pix_pos, self.f_flow, self.f_point_cloud_2, self.f_motion_map = motion_net(self.I_t0, self.I_t1,
-                                                                                          self.f_point_cloud_1)
+                                                                                          self.f_point_cloud_1,
+                                                                                          is_training=self.is_training)
         self.b_pix_pos, self.b_flow, self.b_point_cloud_2, self.b_motion_map = motion_net(self.I_t1, self.I_t0,
                                                                                           self.b_point_cloud_1,
+                                                                                          is_training=self.is_training,
                                                                                           reuse=tf.AUTO_REUSE)
 
-        with tf.variable_scope('loss'):
-            self.f_frame_loss = get_frame_loss()(self.I_t0, self.I_t1, self.f_pix_pos)
-            self.b_frame_loss = get_frame_loss()(self.I_t1, self.I_t0, self.b_pix_pos, reuse=tf.AUTO_REUSE)
+        self.f_frame_loss = get_frame_loss()(self.I_t0, self.I_t1, self.f_pix_pos)
+        self.b_frame_loss = get_frame_loss()(self.I_t1, self.I_t0, self.b_pix_pos, reuse=tf.AUTO_REUSE)
 
-            self.f_flow_sm_loss = get_smooth_loss(order=1)(self.f_flow, 'flow')
-            self.b_flow_sm_loss = get_smooth_loss(order=1)(self.b_flow, 'flow', reuse=tf.AUTO_REUSE)
+        self.f_flow_sm_loss = get_smooth_loss(order=1)(self.f_flow, 'flow')
+        self.b_flow_sm_loss = get_smooth_loss(order=1)(self.b_flow, 'flow', reuse=tf.AUTO_REUSE)
 
-            self.f_depth_sm_loss = get_smooth_loss(order=1)(self.f_depth_output, 'depth')
-            self.b_depth_sm_loss = get_smooth_loss(order=1)(self.b_depth_output, 'depth', reuse=tf.AUTO_REUSE)
+        self.f_depth_sm_loss = get_smooth_loss(order=1)(self.f_depth_output, 'depth')
+        self.b_depth_sm_loss = get_smooth_loss(order=1)(self.b_depth_output, 'depth', reuse=tf.AUTO_REUSE)
 
-            self.f_motion_sm_loss = get_smooth_loss(order=1)(self.f_motion_map, 'motion')
-            self.b_motion_sm_loss = get_smooth_loss(order=1)(self.b_motion_map, 'motion', reuse=tf.AUTO_REUSE)
+        self.f_motion_sm_loss = get_smooth_loss(order=1)(self.f_motion_map, 'motion')
+        self.b_motion_sm_loss = get_smooth_loss(order=1)(self.b_motion_map, 'motion', reuse=tf.AUTO_REUSE)
 
-            self.f_depth_loss = get_fb_depth_loss()(self.f_depth_output, self.b_depth_output, self.f_pix_pos,
-                                                    self.f_point_cloud_1)
-            self.b_depth_loss = get_fb_depth_loss()(self.b_depth_output, self.f_depth_output, self.b_pix_pos,
-                                                    self.b_point_cloud_1, reuse=tf.AUTO_REUSE)
+        self.f_depth_loss = get_fb_depth_loss()(self.f_depth_output, self.b_depth_output, self.f_pix_pos,
+                                                self.f_point_cloud_1)
+        self.b_depth_loss = get_fb_depth_loss()(self.b_depth_output, self.f_depth_output, self.b_pix_pos,
+                                                self.b_point_cloud_1, reuse=tf.AUTO_REUSE)
 
-            self.total_loss = self.f_depth_loss + self.f_depth_sm_loss + self.f_motion_sm_loss + self.f_flow_sm_loss + \
-                              self.f_frame_loss + self.b_depth_loss + self.b_depth_sm_loss + self.b_motion_sm_loss + \
-                              self.b_flow_sm_loss + self.b_frame_loss
+        self.total_loss = self.f_depth_loss + self.f_depth_sm_loss + self.f_motion_sm_loss + self.f_flow_sm_loss + \
+                          self.f_frame_loss + self.b_depth_loss + self.b_depth_sm_loss + self.b_motion_sm_loss + \
+                          self.b_flow_sm_loss + self.b_frame_loss
 
-            self.train_op = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate,
-                                                   beta1=self.config.beta1).minimize(self.total_loss)
+        self.train_op = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate,
+                                               beta1=self.config.beta1).minimize(self.total_loss)
 
         print("Model graph was build..")
 
